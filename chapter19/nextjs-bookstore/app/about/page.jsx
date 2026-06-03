aca61baea5ae1264b5cff84ddb2808cf70e57d5c
Malcom-Yingwani <nsovomalcom@gmail.com>
2026-06-03 21:11:23 +0200

    add chapter 19

---
+++ chapter19/nextjs-bookstore/app/about/page.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return <div>About Page</div>;
+};
+export default AboutPage;
+
+export const metadata = {
+  title: "About Greg's Portfolio of Small Bets",
+  description: "We make money",
+  keywords: "money",
+};
