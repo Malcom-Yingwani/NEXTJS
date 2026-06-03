980c4f51e480ed9aa2fa4d43e9e2f0833b68e9ed
Malcom-Yingwani <nsovomalcom@gmail.com>
2026-06-03 22:01:10 +0200

    add chapter 22

---
+++ chapter22/nextjs-bookstore/app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Books from "./components/Books";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Books />
+    </div>
+  );
+};
+
+export default HomePage;
