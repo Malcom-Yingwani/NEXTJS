8da351bac1d800365613be4bc5a5844a34cd79e5
Malcom-Yingwani <nsovomalcom@gmail.com>
2026-06-09 21:06:28 +0200

    add chapter 28

---
+++ chapter28/nextjs-bookstore/app/api/books/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../db";
+
+export async function GET(req) {
+  const books = await prisma.book.findMany();
+  console.log("GET books called");
+  return NextResponse.json(books);
+}
+
+export async function POST(req) {
+  const { title, link, img } = await req.json();
+  await prisma.book.create({
+    data: {
+      title: title,
+      link: link,
+      img: img,
+    },
+  });
+
+  return NextResponse.json("Book added successfully");
+}
